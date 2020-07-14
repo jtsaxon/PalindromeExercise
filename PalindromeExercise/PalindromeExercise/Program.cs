b345a19d4f782d9b060422af832497cf9aecf5f3
jtsaxon <jaketsaxon@gmail.com>
2020-07-14 14:56:42 -0500

    Added WordSmith Class and Created Test Method

--- PalindromeExercise/PalindromeExercise/Program.cs
+++ PalindromeExercise/PalindromeExercise/Program.cs
@@ -2,7 +2,7 @@
 
 namespace PalindromeExercise
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,18 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("civic", true)]
+        [InlineData("madam", true)]
+        [InlineData("baseball", false)]
+        public void Test1(string word, bool expected)
         {
+            var palindrome = new WordSmith();
+
+            var actual = palindrome.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
 
         }
     }
